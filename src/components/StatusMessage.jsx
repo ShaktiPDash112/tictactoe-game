8b3fc1b5877c0c3ceb76c2ca7f88b71341182d6f
Shakti Prasad Dash <dashshakti1@gmail.com>
2023-03-13 12:59:48 +0530

    display draw message. applied style for status message component

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,44 @@
+const StatusMessage = ({ winner, isXNext, squares }) => {
+  const noMovesLeft = squares.every(squareValue => squareValue !== null);
+  const nextPlayer = isXNext ? '1' : '2';
+
+  const renderStatusMessage = () => {
+    if (winner) {
+      return (
+        <>
+          Winner is{' '}
+          <span className={isXNext ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      );
+    }
+    if (!winner && noMovesLeft) {
+      return (
+        <>
+          {' '}
+          Player <span className="text-green">1</span> and Player{' '}
+          <span className="text-orange">2</span> ! Tied
+        </>
+      );
+    }
+
+    if (!winner && !noMovesLeft) {
+      return (
+        <>
+          {' '}
+          Player{' '}
+          <span className={isXNext ? 'text-green' : 'text-orange'}>
+            {nextPlayer}
+          </span>
+          `s turn
+        </>
+      );
+    }
+
+    return null;
+  };
+  return <h2 className="status-message">{renderStatusMessage()}</h2>;
+};
+
+export default StatusMessage;

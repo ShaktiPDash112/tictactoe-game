0b82be795fcc74c801fb45736859e610d4fb6a97
Shakti Prasad Dash <dashshakti1@gmail.com>
2023-03-17 20:24:18 +0530

    added Game Reset button

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,12 @@ function App() {
   const moveTo = move => {
     setCurrentMove(move);
   };
+
+  const onNewGameStart = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
   return (
     <div className="app">
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
@@ -58,6 +64,14 @@ function App() {
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
       />
+      <button
+        type="button"
+        onClick={onNewGameStart}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
+        Start New Game
+      </button>
+
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
